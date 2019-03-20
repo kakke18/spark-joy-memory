2072bbe0c767931e7801cde719669ab493bb3f54
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-03-21 06:19:49 +0900

    add: line chart

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const testBtn2 = document.getElementById('test2-btn')
 const testBtn3 = document.getElementById('test3-btn')
 const testBtn4 = document.getElementById('test4-btn')
 const mainContainer = document.getElementById('main-container')
+const canvas = document.createElement('canvas');
 
 const MAX_LAUGH_STG = 1.0
 const MAX_RARE_ENCOUNT_POINT = 5
@@ -31,7 +32,6 @@ testBtn2.addEventListener('click', () => {
     .then((res) => {
       console.log(res.data)
       removeAllChildren(mainContainer)
-      const canvas = document.createElement('canvas');
       createRadarChart(canvas, res.data.user_points)
       mainContainer.appendChild(canvas)
     })
@@ -44,6 +44,9 @@ testBtn3.addEventListener('click', () => {
     axios.get(host + ':' + port + '/test3')
     .then((res) => {
       console.log(res.data)
+      removeAllChildren(mainContainer)
+      createLineChart(canvas, res.data.bunch_scores, res.data.user_scores)
+      mainContainer.appendChild(canvas)
     })
     .catch((err) => {
       console.error(err)
@@ -101,9 +104,9 @@ const createRadarChart = (canvas, user_points) => {
         text: '個人成績'
       },
       // 凡例の設定
-      legend: {
-        position: 'left'
-      },
+      // legend: {
+      //   position: 'left'
+      // },
       responsive: true,
       scale: {
         // point名のfsize
@@ -133,4 +136,73 @@ const normalizeUserPoints = (user_points) => {
   calcResults.push(user_points.between_product_interact_point / MAX_BETWEEN_PRODUCT_INTERACT_POINT * 100)
   calcResults.push(user_points.diversity_point / MAX_DIVERSITY_POINT * 100)
   return calcResults
+}
+
+const createLineChart = (canvas, bunch_scores, user_scores) => {
+  const labels = []
+  const bunch_datas = []
+  const user_datas = []
+  for (let i = 0; i < bunch_scores.length; i++) {
+    labels.push(bunch_scores[i].date)
+    bunch_datas.push(bunch_scores[i].score)
+  }
+  for (let i = 0; i < user_scores.length; i++) {
+    user_datas.push(user_scores[i].score)
+  }
+  console.log(bunch_datas)
+  console.log(user_datas)
+
+  new Chart(canvas, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'バンチ成績',
+          data: bunch_datas,
+          backgroundColor: 'RGB(254,213,68)',
+          borderColor: 'RGB(254,213,68)',
+          borderWidth: 4,
+          lineTension: 0,
+          fill: false
+        },
+        {
+          label: '個人成績',
+          data: user_datas,
+          backgroundColor: 'RGB(255,127,150)',
+          borderColor: 'RGB(255,127,150)',
+          borderWidth: 4,
+          lineTension: 0,
+          fill: false
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: '成績'
+      },
+      // 凡例の設定
+      // legend: {
+      //   position: 'left'
+      // },
+      responsive: true,
+      scales: {
+        xAxes: [{
+          ticks: {
+            fontSize: 12
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            callback: () => {
+              return ''
+            },
+            beginAtZero: true,
+            max: 100
+          }
+        }]
+      }
+    }
+  })
 }
